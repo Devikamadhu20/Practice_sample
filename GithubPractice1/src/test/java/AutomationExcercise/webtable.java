4efd488e398aaa18ebe68a143608e3abc14d6fb4
Devikamadhu20 <devikakushvik@gmail.com>
2026-08-11 20:21:47 +0530

    3rd commit

--- GithubPractice1/src/test/java/AutomationExcercise/webtable.java
+++ GithubPractice1/src/test/java/AutomationExcercise/webtable.java
@@ -3,6 +3,8 @@ package AutomationExcercise;
 public class webtable {
 	 public static void main(String[] args) {
 		System.out.println("Hiiiii");
+		System.out.println("Hello");
+		System.out.println("hiii helloooo");
 	}
 
 }
