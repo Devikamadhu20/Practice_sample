17abd6d9b262bc4d3a55e7759870b8b752423805
Devikamadhu20 <devikakushvik@gmail.com>
2026-08-11 20:45:14 +0530

    devika to devika

--- GithubPractice1/src/test/java/AutomationExcercise/webtable.java
+++ GithubPractice1/src/test/java/AutomationExcercise/webtable.java
@@ -4,7 +4,7 @@ public class webtable {
 	 public static void main(String[] args) {
 		System.out.println("Hiiiii");
 		System.out.println("Hello");
-		System.out.println("hiii helloooo");
+		System.out.println("hey am devika");
 	}
 
 }
